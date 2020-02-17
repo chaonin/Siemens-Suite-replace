5c8440beb841345a6bae51856fee4173e43b6645
chaonin <chaonin@gmail.com>
2020-02-17 22:18:16 +0800

    all

--- clean.sh
+++ clean.sh
@@ -1,3 +1,3 @@
 cd ./scripts && rm -rf run_v*
-cd ../outputs && rm t* #rm all outputs of replace from original version
+cd ../outputs && rm -rf t* #rm all outputs of replace from original version
 cd ../newoutputs && rm -rf v* #rm all outputs of all versions
