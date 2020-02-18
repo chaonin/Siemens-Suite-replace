bb88dc11d5c4cc7be5c620d836456c5a6a2db289
chaonin <chaonin@gmail.com>
2020-02-18 17:03:50 +0800

    a

--- clean.sh
+++ clean.sh
@@ -2,3 +2,4 @@ cd ./scripts && rm -rf run_v*
 cd ../outputs && rm -rf t* #rm all outputs of replace from original version
 cd ../newoutputs && rm -rf v* #rm all outputs of all versions
 cd ../ && rm -rf spectrum/ # rm all spectrum related info 
+cd versions.alt/versions.orig/v1/ && rm -rf tra* # rm all output of dir "tra" (for pvtrace), current just do version 1
